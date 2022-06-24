a1fc9e3316673bbeedd4b397dc2ccbde9331a373
Julev Ajeto <jersoncarin25@gmail.com>
2022-06-24 18:44:14 +0800

    Initial build

---
+++ overlay.mk
@@ -0,0 +1,22 @@
+PRODUCT_PACKAGES += \
+	HardwareOverlayPicker \
+	QtiAudio \
+	TrebleApp \
+	treble-overlay-NavBar \
+	treble-overlay-NightMode \
+	treble-overlay-SystemUI-FalseLocks \
+	treble-overlay-Telephony-LTE \
+	treble-overlay-devinputjack \
+	treble-overlay-highpriomisc \
+	treble-overlay-misc-aod \
+	treble-overlay-misc-aod-systemui \
+	treble-overlay-misc-dt2w \
+	treble-overlay-misc-minimal-brightness \
+	treble-overlay-mtk-ims \
+	treble-overlay-telephony-mtk-ims \
+	treble-overlay-tethering \
+	treble-overlay-wifi5g \
+	treble-overlay-tethering-nobpf \
+	treble-overlay-xiaomi-pocox3gt \
+	treble-overlay-xiaomi-pocox3gt-systemui \
+
